7575bbca5d56b402d24e3ba8e9e0b92e60ea939d
realabdullah <abdulodesanmi@gmail.com>
2022-02-10 01:17:47 +0100

    removed unused icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/css/style.css'
+    '~/assets/css/style.css',
+    '~/assets/css/normalize.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
