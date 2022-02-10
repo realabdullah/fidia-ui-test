1f207c560a5e68cf00dd16a2d14960e4d5c4ac36
realabdullah <abdulodesanmi@gmail.com>
2022-02-10 01:08:16 +0100

    style.css

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'fidia-test',
+    title: 'FIDIA UI DASHBOARD',
     htmlAttrs: {
       lang: 'en'
     },
@@ -24,6 +24,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/css/style.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
