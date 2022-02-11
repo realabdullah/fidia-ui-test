dfba35fb61844a6a1abad98add9a344645cb0497
realabdullah <abdulodesanmi@gmail.com>
2022-02-11 13:50:56 +0100

    css adjustments

--- components/Filters.vue
+++ components/Filters.vue
@@ -24,6 +24,13 @@
         </svg>
         <span>More filters</span>
       </button>
+      <button class="edit__filters">
+        <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M4 6H14M1.5 1H16.5M6.5 11H11.5" stroke="#344054" stroke-width="1.67" stroke-linecap="round"
+            stroke-linejoin="round" />
+        </svg>
+        <span>Edit filters</span>
+      </button>
     </div>
     <div class="__search">
       <label class="sr-only" for="search">Search</label>
