205286e83c278b2a8fdc4044da23c4f54e22611f
realabdullah <abdulodesanmi@gmail.com>
2022-02-13 08:34:58 +0100

    adjusted form icon

--- components/Header.vue
+++ components/Header.vue
@@ -169,13 +169,16 @@
         </nav>
       </div>
       <div class="nav__sub__four">
-        <label class="sr-only" for="search">Search</label>
-        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M16.5 16.5L12.875 12.875M14.8333 8.16667C14.8333 11.8486 11.8486 14.8333 8.16667 14.8333C4.48477 14.8333 1.5 11.8486 1.5 8.16667C1.5 4.48477 4.48477 1.5 8.16667 1.5C11.8486 1.5 14.8333 4.48477 14.8333 8.16667Z"
-            stroke="#667085" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
-        </svg>
-        <input type="search" name="search" placeholder="Search">
+        <form>
+          <label for="searc">
+            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M16.5 16.5L12.875 12.875M14.8333 8.16667C14.8333 11.8486 11.8486 14.8333 8.16667 14.8333C4.48477 14.8333 1.5 11.8486 1.5 8.16667C1.5 4.48477 4.48477 1.5 8.16667 1.5C11.8486 1.5 14.8333 4.48477 14.8333 8.16667Z"
+                stroke="#667085" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+          </label>
+          <input type="search" id="searc" placeholder="Search">
+        </form>
       </div>
     </div>
   </header>
